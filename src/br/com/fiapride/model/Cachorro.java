f8f9e848fa8bf48fa02886cfa4a3098b28abdd85
lU4N4ht <luana.maga78@gmail.com>
2026-02-19 19:38:36 -0300

    criação de classe e instanciação no main

---
+++ src/br/com/fiapride/model/Cachorro.java
@@ -0,0 +1,10 @@
+package br.com.fiapride.model;
+
+public class Cachorro {
+
+    // Atributos
+    public String raca, nome, humor;
+    public double peso;
+    public int idade;
+    public boolean estaAcordado;
+}
