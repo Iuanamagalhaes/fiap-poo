692cd543973aab85fbbc81f7feb246f97d18927a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-26 11:11:07 -0300

    métodos adicionados

--- src/br/com/fiapride/main/Main.java
+++ src/br/com/fiapride/main/Main.java
@@ -1,31 +1,23 @@
 package br.com.fiapride.main;
 import br.com.fiapride.model.Cachorro;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         // Criando o primeiro objeto cachorro
-        Cachorro cachorro1 = new Cachorro();
-        cachorro1.nome = "Bidu";
-        cachorro1.raca = "Beagle";
-        cachorro1.idade = 3;
-        cachorro1.peso = 10.5;
-        cachorro1.estaAcordado = true;
-        cachorro1.humor = "Feliz";
+        Cachorro cachorro1 = new Cachorro("Bidu", "Beagle","Feliz", 10.5, 3);
 
-        // Criando o segundo objeto cachorro
-        Cachorro cachorro2 = new Cachorro();
-        cachorro2.nome = "Lulu";
-        cachorro2.raca = "Poodle";
-        cachorro2.idade = 10;
-        cachorro2.peso = 6.0;
-        cachorro2.estaAcordado = false;
-        cachorro2.humor = "Sonolenta";
+        cachorro1.acordarCachorro();
+        cachorro1.verificarIdoso();
+        cachorro1.latir();
 
-        // Exibindo
-        System.out.println("Cachorro: " + cachorro1.nome + " | Raça: " + cachorro1.raca + " | Idade: " + cachorro1.idade);
-        System.out.println("Cachorro: " + cachorro2.nome + " | Raça: " + cachorro2.raca + " | Idade: " + cachorro2.idade);
+//        Scanner scanner = new Scanner(System.in);
+//        // Exibindo
+//        System.out.println("Cachorro: " + cachorro1.nome + " | Raça: " + cachorro1.raca + " | Idade: " + cachorro1.idade);
+
 
     }
 }
--- src/br/com/fiapride/model/Cachorro.java
+++ src/br/com/fiapride/model/Cachorro.java
@@ -7,4 +7,41 @@ public class Cachorro {
     public double peso;
     public int idade;
     public boolean estaAcordado;
+
+    public Cachorro(String nome, String raca, String humor, double peso, int idade){
+        this.nome = nome;
+        this.raca = raca;
+        this.humor = humor;
+        this.peso = peso;
+        this.idade = idade;
+        this.estaAcordado = false;
+    }
+
+    public void latir(){
+        if (estaAcordado == false){
+            System.out.println("Erro: O cachorro está dormindo.");
+            return;
+        }
+        System.out.println("Au!");
+    }
+
+    public void acordarCachorro(){
+        if (estaAcordado == true){
+            System.out.println("Erro: o cachorro já está acordado.");
+            return;
+        }
+        this.estaAcordado = true;
+        System.out.println("Acordei");
+    }
+
+    public void verificarIdoso(){
+        if(idade < 7){
+            System.out.println("O cachorro tem " + idade + " anos, ainda não é idoso.");
+            return;
+        }
+        System.out.println("O cachorro tem " + idade + " anos, já é considerado idoso.");
+    }
+
 }
+
+
