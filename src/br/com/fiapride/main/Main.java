69f25011f6cfd0e7fb9229b1ed9d8a59d7baf269
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-26 11:19:24 -0300

    implementação menu

--- src/br/com/fiapride/main/Main.java
+++ src/br/com/fiapride/main/Main.java
@@ -1,23 +1,11 @@
 package br.com.fiapride.main;
-import br.com.fiapride.model.Cachorro;
 
-import java.util.Scanner;
+import br.com.fiapride.model.Menu;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        // Criando o primeiro objeto cachorro
-        Cachorro cachorro1 = new Cachorro("Bidu", "Beagle","Feliz", 10.5, 3);
-
-        cachorro1.acordarCachorro();
-        cachorro1.verificarIdoso();
-        cachorro1.latir();
-
-//        Scanner scanner = new Scanner(System.in);
-//        // Exibindo
-//        System.out.println("Cachorro: " + cachorro1.nome + " | Raça: " + cachorro1.raca + " | Idade: " + cachorro1.idade);
-
-
+        Menu menu = new Menu();
+        menu.escolhaMenu();
     }
 }
---
+++ src/br/com/fiapride/model/Menu.java
@@ -0,0 +1,42 @@
+package br.com.fiapride.model;
+
+import java.util.Scanner;
+
+public class Menu {
+    Scanner scanner = new Scanner(System.in);
+    Cachorro cachorro = new Cachorro("Bidu", "Beagle","Feliz", 10.5, 3);
+    boolean sair = false;
+
+    public void escolhaMenu() {
+
+        while (!sair) {
+            System.out.println("+______________________________________________________+");
+            System.out.println("| Escolha uma opção....................................| ");
+            System.out.println("| 1 - Latir............................................|");
+            System.out.println("| 2 - Acordar cachorro.................................|");
+            System.out.println("| 3 - Verificar se está idoso..........................|");
+            System.out.println("| 5 - Sair.............................................|");
+            System.out.println("+______________________________________________________+");
+            int escolha = scanner.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    cachorro.latir();
+                    break;
+                case 2:
+                    cachorro.acordarCachorro();
+                    break;
+                case 3:
+                    cachorro.verificarIdoso();
+                    break;
+                case 5:
+                    sair = true;
+                    break;
+            }
+        }
+    }
+
+
+
+
+}
