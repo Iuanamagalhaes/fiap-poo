44a300c6ad87e533b3863c30f05f623def3e4ee7
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-06 10:12:04 -0300

    implmentação inicial get e set

--- src/br/com/fiapride/model/Cachorro.java
+++ src/br/com/fiapride/model/Cachorro.java
@@ -3,18 +3,18 @@ package br.com.fiapride.model;
 public class Cachorro {
 
     // Atributos
-    public String raca, nome, humor;
-    public double peso;
-    public int idade;
-    public boolean estaAcordado;
+    private String raca, nome, humor;
+    private double peso;
+    private int idade;
+    private boolean estaAcordado;
 
-    public Cachorro(String nome, String raca, String humor, double peso, int idade){
-        this.nome = nome;
-        this.raca = raca;
-        this.humor = humor;
-        this.peso = peso;
-        this.idade = idade;
-        this.estaAcordado = false;
+    public Cachorro(){
+        this.setNome(nome);
+        this.setRaca(raca);
+        this.setHumor(humor);
+        this.setPeso(peso);
+        this.setIdade(idade);
+        this.setEstaAcordado(false);
     }
 
     public void latir(){
@@ -25,14 +25,16 @@ public class Cachorro {
         System.out.println("Au!");
     }
 
+
     public void acordarCachorro(){
         if (estaAcordado == true){
             System.err.println("Erro: O cachorro está dormindo.");
             return;
         }
-        this.estaAcordado = true;
+        setEstaAcordado(true);
         System.out.println("Acordei");
     }
+
 
     public void verificarIdoso(){
         if(idade < 7){
@@ -42,6 +44,49 @@ public class Cachorro {
         System.out.println("O cachorro tem " + idade + " anos, já é considerado idoso.");
     }
 
+    public String getNome() {
+        return this.nome;
+    }
+
+    private void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return this.raca;
+    }
+
+    private void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getHumor() {
+        return this.humor;
+    }
+
+    public void setHumor(String humor) {
+        this.humor = humor;
+    }
+
+    public double getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setEstaAcordado(boolean estaAcordado) {
+        this.estaAcordado = estaAcordado;
+    }
 }
 
 
--- src/br/com/fiapride/model/Menu.java
+++ src/br/com/fiapride/model/Menu.java
@@ -4,37 +4,36 @@ import java.util.Scanner;
 
 public class Menu {
     Scanner scanner = new Scanner(System.in);
-    Cachorro cachorro = new Cachorro("Bidu", "Beagle","Feliz", 10.5, 3);
     boolean sair = false;
 
     public void escolhaMenu() {
 
-        while (!sair) {
-            System.out.println("+______________________________________________________+");
-            System.out.println("| Escolha uma opção....................................| ");
-            System.out.println("| 1 - Latir............................................|");
-            System.out.println("| 2 - Acordar cachorro.................................|");
-            System.out.println("| 3 - Verificar se está idoso..........................|");
-            System.out.println("| 5 - Sair.............................................|");
-            System.out.println("+______________________________________________________+");
-            System.out.print("Escolha: ");
-            int escolha = scanner.nextInt();
-
-            switch (escolha) {
-                case 1:
-                    cachorro.latir();
-                    break;
-                case 2:
-                    cachorro.acordarCachorro();
-                    break;
-                case 3:
-                    cachorro.verificarIdoso();
-                    break;
-                case 5:
-                    sair = true;
-                    break;
-            }
-        }
+//        while (!sair) {
+//            System.out.println("+______________________________________________________+");
+//            System.out.println("| Escolha uma opção....................................| ");
+//            System.out.println("| 1 - Latir............................................|");
+//            System.out.println("| 2 - Acordar cachorro.................................|");
+//            System.out.println("| 3 - Verificar se está idoso..........................|");
+//            System.out.println("| 5 - Sair.............................................|");
+//            System.out.println("+______________________________________________________+");
+//            System.out.print("Escolha: ");
+//            int escolha = scanner.nextInt();
+//
+//            switch (escolha) {
+//                case 1:
+//                    cachorro.latir();
+//                    break;
+//                case 2:
+//                    cachorro.acordarCachorro();
+//                    break;
+//                case 3:
+//                    cachorro.verificarIdoso();
+//                    break;
+//                case 5:
+//                    sair = true;
+//                    break;
+//            }
+//        }
     }
 
 
